7752bfa0427d18c69f8069e88d0fd5464a36304e
Morris Clay <mail@morrisclay.com>
2021-06-15 17:32:15 +0200

    inital commit

--- api/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'a1b9964374a29bdf1539ff6d687c92f7'),
-    },
-  },
-});
--- api/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '88505248-3ef6-4c00-b90c-7246876330ef'
-};
